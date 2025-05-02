9a6f00347944a514e783140be20eced32b82a36e
Edoardo Vacchi <evacchi@users.noreply.github.com>
2025-05-02 08:45:28 +0200

    feat: support mid-turn advancement (#6)
    Signed-off-by: Edoardo Vacchi <evacchi@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-import type { ContentBlockParam, MessageCreateParamsNonStreaming, Tool } from '@anthropic-ai/sdk/resources/index.js'
+import type {
+  ContentBlockParam,
+  MessageCreateParamsNonStreaming,
+  Tool,
+  ToolUseBlock,
+} from '@anthropic-ai/sdk/resources/index.js'
 import type { Logger, SessionOptions } from '@dylibso/mcpx'
 import { Session } from '@dylibso/mcpx'
 import type { RequestOptions } from '@anthropic-ai/sdk/core'
-import type { Anthropic, BadRequestError } from '@anthropic-ai/sdk'
+import { Anthropic, BadRequestError } from '@anthropic-ai/sdk'
 import { pino } from 'pino'
 
 export interface BaseDriverOptions {
@@ -15,12 +20,23 @@ export type DriverOptions = (
   (BaseDriverOptions & { sessionOptions?: SessionOptions, sessionId: string, profile?: string })
 )
 
+export interface McpxAnthropicTurn {
+  response: Anthropic.Messages.Message
+  messages: Anthropic.Messages.MessageParam[]
+  index: number
+  toolCallIndex?: number
+  done: boolean
+}
+
 export interface McpxAnthropicStage {
   response: Anthropic.Messages.Message
   messages: Anthropic.Messages.MessageParam[]
   index: number
-  done: boolean
+  toolCallIndex?: number
+  submessageIdx?: number
+  status: 'ready' | 'pending' | 'input_wait'
 }
+
 
 /**
  * A Driver wrapping an Anthropic client and MCPX session.
@@ -66,91 +82,165 @@ export class Driver {
     return response
   }
 
-  async nextTurn(body: MessageCreateParamsNonStreaming, messageIdx: number, options: RequestOptions): Promise<McpxAnthropicStage> {
+  async nextTurn(body: MessageCreateParamsNonStreaming, messageIdx: number, options: RequestOptions): Promise<McpxAnthropicTurn> {
+
     let { messages, ...rest } = body
-    let response: Anthropic.Messages.Message
-    try {
-      response = await this.#anthropic.messages.create({
-        ...rest,
-        ...(this.#tools.length ? { tools: this.#tools } : {}),
-        messages,
-      }, options)
-    } catch (err: any) {
-      throw ToolSchemaError.parse(err)
+
+    let stage: McpxAnthropicStage = {
+      messages,
+      index: messageIdx,
+      status: 'pending',
+      response: {} as Anthropic.Messages.Message,
     }
-
-    messages.push({
-      role: response.role,
-      content: response.content,
-    })
-
-    for (; messageIdx < messages.length; ++messageIdx) {
-      this.#logger.info({ exchange: messages[messageIdx] }, 'message')
+    stage = await this.next(stage, rest, options)
+    switch (stage.status) {
+      case 'ready':
+        return {
+          messages: stage.messages,
+          index: stage.index,
+          response: stage.response,
+          done: true,
+        }
+      case 'pending':
+        return {
+          messages: stage.messages,
+          index: stage.index,
+          response: stage.response,
+          done: false,
+        }
+      case 'input_wait':
+        do {
+          stage = await this.next(stage, rest, options)
+        } while (stage.status === 'input_wait')
+        return {
+          messages: stage.messages,
+          index: stage.index,
+          response: stage.response,
+          done: stage.status === 'ready',
+        }
     }
-
-    const newMessage = { role: 'user' as const, content: [] as ContentBlockParam[] }
-    messages.push(newMessage)
-    let toolUseCount = 0
-    for (const submessage of response.content) {
-      if (submessage.type !== 'tool_use') {
-        continue
-      }
-
-      ++toolUseCount
-      const { id, input, name } = submessage
-      try {
-        const abortcontroller = new AbortController()
-        const result = await this.#session.handleCallTool(
-          {
-            method: 'tools/call',
-            params: {
-              name,
-              arguments: input as any
-            },
-          },
-          { signal: abortcontroller.signal }
-        )
-
-        newMessage.content.push({
-          tool_use_id: id,
-          type: 'tool_result',
-          content: Array.isArray(result.content)
-            ? result.content.map(xs => {
-              return { [xs.type]: xs[xs.type], type: xs.type }
-            }) as any
-            : result.content
-        })
-      } catch (err: any) {
-        this.#logger.error(
-          {
-            tool_use_id: id,
-            name,
-            error: err.message,
-            stack: err.stack,
-          },
-          'tool use failed'
-        )
-        newMessage.content.push({
-          tool_use_id: id,
-          type: 'tool_result',
-          content: err.toString(),
-          is_error: true
-        })
-      }
-    }
-
-    if (response.stop_reason === 'tool_use') {
-      return { response,  messages, index: messageIdx, done: false }
-    }
-
-    if (response.stop_reason === 'end_turn' && toolUseCount > 0) {
-      return { response,  messages, index: messageIdx, done: false }
-    }
-
-    messages.pop()
-    this.#logger.info({ lastMessage: messages[messages.length - 1], stopReason: response.stop_reason }, 'final message')
-    return { response,  messages, index: messageIdx, done: true }
   }
+
+
+  private async call(submessage: ToolUseBlock): Promise<ContentBlockParam> {
+    const { id, input, name } = submessage
+    try {
+      const abortcontroller = new AbortController()
+      const result = await this.#session.handleCallTool(
+        {
+          method: 'tools/call',
+          params: {
+            name,
+            arguments: input as any,
+          },
+        },
+        { signal: abortcontroller.signal },
+      )
+
+      return {
+        tool_use_id: id,
+        type: 'tool_result',
+        content: Array.isArray(result.content)
+          ? result.content.map(xs => {
+            return { [xs.type]: xs[xs.type], type: xs.type }
+          }) as any
+          : result.content,
+      }
+    } catch (err: any) {
+      this.#logger.error(
+        {
+          tool_use_id: id,
+          name,
+          error: err.message,
+          stack: err.stack,
+        },
+        'tool use failed',
+      )
+      return {
+        tool_use_id: id,
+        type: 'tool_result',
+        content: err.toString(),
+        is_error: true,
+      }
+    }
+  }
+
+  async next(stage: McpxAnthropicStage, config: any, requestOptions?: RequestOptions<unknown>): Promise<McpxAnthropicStage> {
+    const { response, messages, index, status, toolCallIndex } = stage
+    switch (status) {
+      case 'pending': {
+        let response: Anthropic.Messages.Message
+        try {
+          response = await this.#anthropic.messages.create({
+            ...config,
+            ...(this.#tools.length ? { tools: this.#tools } : {}),
+            messages,
+          }, requestOptions)
+        } catch (err: any) {
+          throw ToolSchemaError.parse(err, this.#tools)
+        }
+
+        messages.push({
+          role: response.role,
+          content: response.content,
+        })
+
+        let messageIdx = index
+        for (; messageIdx < messages.length; ++messageIdx) {
+          this.#logger.info({ exchange: messages[messageIdx] }, 'message')
+        }
+        const newMessage = { role: 'user' as const, content: [] as ContentBlockParam[] }
+        messages.push(newMessage)
+        let toolUseCount = 0
+        let submessageIdx = 0;
+        for (const submessage of response.content) {
+          if (submessage.type === 'tool_use') {
+            return { response, messages, index: messageIdx, status: 'input_wait', toolCallIndex: toolUseCount, submessageIdx }
+          }
+          submessageIdx++
+        }
+
+        if (response.stop_reason === 'tool_use') {
+          return { response,  messages, index: messageIdx, status: 'pending' }
+        }
+
+        if (response.stop_reason === 'end_turn' && toolCallIndex !== undefined && toolCallIndex > 0) {
+          return { response,  messages, index: messageIdx, status: 'pending' }
+        }
+
+        messages.pop()
+        this.#logger.info({ lastMessage: messages[messages.length - 1], stopReason: response.stop_reason }, 'final message')
+        return { response,  messages, index: messageIdx, status: 'ready' }
+      }
+      case 'input_wait': {
+        const toolUseCount = stage.toolCallIndex!
+        const submessageIdx = stage.submessageIdx!
+        const inputMessage = messages[index-1]
+        const newMessage = messages[index]
+
+        this.#logger.info({ m:"message index", index, len: messages.length })
+
+        // when status == 'input_wait' it is always a tool call,
+        // newMessage.content is always a ContentBlockParam[]
+        const submessage = inputMessage.content[submessageIdx] as ToolUseBlock
+        const content = newMessage.content as ContentBlockParam[]
+        content.push(await this.call(submessage))
+
+        const nextTool = toolUseCount + 1
+        const nextSubmessage = submessageIdx + 1
+        if (nextSubmessage >= inputMessage.content.length) {
+          return { response, messages, index, status: 'pending' }
+        } else {
+          return { response, messages, index, status: 'input_wait', toolCallIndex: nextTool, submessageIdx: nextSubmessage }
+        }
+
+      }
+      default:
+        throw new Error("Illegal status: " + status)
+    }
+  }
+
+
 }
 
 /** Create a driver using an Anthropic client and MCPX Session options. */
@@ -186,14 +276,14 @@ export default async function createDriver(opts: DriverOptions) {
 }
 
 export class ToolSchemaError extends Error {
-  static parse(err: any): any {
+  static parse(err: any, tools: Tool[]): any {
     const error = err?.error?.error
     const message = error?.message
     if (error?.type === 'invalid_request_error' && message?.includes('input_schema')) {
       if (message.startsWith("tools.")) {
         const parts: string[] = message.split('.')
         const index = parseInt(parts[1])
-        return new ToolSchemaError(err, index)
+        return new ToolSchemaError(err, index, tools[index].name)
       }
     }
     return err
@@ -201,11 +291,12 @@ export class ToolSchemaError extends Error {
 
   public readonly originalError: any
   public readonly toolIndex: number
-  constructor(error: any, toolIndex: number) {
-    super(error.message)
-
+  public readonly toolName: string
+  constructor(error: any, toolIndex: number, toolName: string) {
+    super(`Invalid schema for tool #${toolIndex}: '${toolName}'\nCaused by: ${error.message}`)
     this.originalError = error;
     this.toolIndex = toolIndex
+    this.toolName = toolName
   }
 
 }
